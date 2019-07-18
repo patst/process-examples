e3c3a6174c0514e998f60fb20476f87ebe81bb04
patst <patrick.steinig@googlemail.com>
2019-07-18 11:35:22 +0200

    Add Cockpit Plugin example

---
+++ cockpit-plugin/README.md
@@ -0,0 +1,10 @@
+# Cockpit plugin
+
+Example how to integrate a custom plugin into camunda cockpit.
+
+For this to work build the project with `mvn clean install` and then launch the built jar:
+
+`java -jar ./cockpit/target/cockpit-0.0.1-SNAPSHOT.jar`
+
+
+The plugin is visible on the dashboard view
---
+++ cockpit-plugin/cockpit/src/main/java/de/patst/process/CockpitPluginApplication.java
@@ -0,0 +1,13 @@
+package de.patst.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockpitPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CockpitPluginApplication.class, args);
+    }
+
+}
